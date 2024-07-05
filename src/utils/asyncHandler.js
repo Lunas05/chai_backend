19a2ae7205ce5a1f83fac2766c33559680269dc4
Lunas05 <mohitgogoi02@gmail.com>
2024-07-05 23:24:56 +0530

    utils added and error not solved

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requesthandler) => {
+    (req, res, next) => {
+        Promise.resolve(requesthandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+// const asynchandler = (fn) => {}
+// const asynchandler = (func) => () => {}
+// const asynchandler = (func) => async () => {}
+
+// const asyncHandler = (fn) => async () => {
+//     try {
+//         await fn(req, res, next)
+
+//     }catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
